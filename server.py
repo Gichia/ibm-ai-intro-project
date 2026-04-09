d5d58510fb502e6eca8712e07ffe2baae4b7d121
Peter Gichia <petergichia35@gmail.com>
2026-04-09 09:22:08 +0300

    Add the flask app

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+#Initiate the flask app : TODO
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def sent_analyzer():
+    ''' This function is responsible for receiving the text to be analyzed from the 
+    front end, passing it to the emotion_detector function and returning 
+    the response back to the front end
+    '''
+    text_to_analyze = request.args.get('textToAnalyze')
+    # Pass the text to the emotion_detector function and store the response
+    response = emotion_detector(text_to_analyze)
+
+    return f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
+
+@app.route("/")
+def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    '''This functions executes the flask app and deploys it on localhost:5000'''
+    app.run(host="0.0.0.0", port=5000)
